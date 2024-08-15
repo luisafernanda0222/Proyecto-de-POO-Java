44a6c0052f6dfd23c50dcee2b2adb254c7221243
alejandra gaitan <nicoll_cespedes@soy.sena.edu.co>
2024-08-15 10:40:10 -0500

    primer commit

---
+++ src/edu/misena/senaviewer/model/Chapter.java
@@ -0,0 +1,17 @@
+package edu.misena.senaviewer.model;
+
+import java.util.List;
+
+public class Chapter {
+    int id;
+    String title;
+    String duration;
+    int year;
+    String viewed;
+    int timeViewed;
+    String sessionNumber;
+}
+
+    public Book(String title, String duration,  int year){
+
+    }
